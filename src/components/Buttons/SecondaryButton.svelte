5cc2042b65b6a35d7947d21f89407e79fa885d06
Andreas Würtz <urintrolden@gmail.com>
2024-10-07 14:37:36 +0200

    add primary and secondary buttons

---
+++ src/components/Buttons/SecondaryButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+
+    export let text = "default";
+ 
+</script>
+    
+<Button variant="secondary">{text}</Button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import PrimaryButton from "../components/Buttons/PrimaryButton.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<PrimaryButton text="Primary Button" />
+<PrimaryButton text="Secondary Button" />
