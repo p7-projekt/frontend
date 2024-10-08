99da4e325fb3ed294fdae543a07fd7b651c8af6c
EMojoKrogh <ekrogh18@student.aau.dk>
2024-10-08 12:01:51 +0200

    added alias to component folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import PrimaryButton from "../components/Buttons/PrimaryButton.svelte";
-
+	import PrimaryButton from '$components/Buttons/PrimaryButton.svelte';
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
