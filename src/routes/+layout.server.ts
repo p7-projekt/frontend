5af6adb9c47e8acb0e939f28b3f07a4d63620389
EMojoKrogh <ekrogh18@student.aau.dk>
2024-10-11 07:25:21 +0200

    Changed something

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,7 +6,7 @@ export const load: LayoutServerLoad = ({ cookies }) => {
 	const token = true;
 	// Get user from token (Not implemented)
 	// role: 1: (admin), 2: (instructor) and 3: (guest)
-	const user = { name: 'Kristian', id: 1, role: 2 };
+	const user = { name: 'Kristian', id: 1, role: 3 };
 
 	return {
 		user: token ? user : null
