22ed614550a1d90ee7669aed082b8d073bd640f0
EMojoKrogh <ekrogh18@student.aau.dk>
2024-10-12 12:25:12 +0200

    finished styling of exerciseList component

--- src/components/ExerciseList/ExerciseList.svelte
+++ src/components/ExerciseList/ExerciseList.svelte
@@ -8,18 +8,48 @@
 			{title_name} Exercises
 		</div>
 	</div>
-	<div class="pl-1">
-		<div class="w-[675px] h-[52px] border-b-[1.5px] flex items-center">
-			Create Hello World Function
+	<div class="text-sm font-medium">
+		<div
+			class="pl-1 pr-1 w-[675px] h-[52px] border-b-[1.5px] flex items-center hover-effect w-full justify-between"
+		>
+			<p class="inline-block">Create Hello World Function</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="#1f2937"
+				class="size-6 cursor-pointer"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+				/>
+			</svg>
 		</div>
-		<div class="w-[675px] h-[52px] border-b-[1.5px] flex items-center">Add two numbers</div>
-		<div class="w-[675px] h-[52px] border-b-[1.5px] flex items-center">Counter</div>
-		<div class="w-[675px] h-[52px] border-b-[1.5px] flex items-center">Counter 2</div>
+
+		<div class="pl-1 w-[675px] h-[52px] border-b-[1.5px] flex items-center hover-effect">
+			Add two numbers
+		</div>
+		<div class="pl-1 w-[675px] h-[52px] border-b-[1.5px] flex items-center hover-effect">
+			Counter
+		</div>
+		<div class="pl-1 w-[675px] h-[52px] border-b-[1.5px] flex items-center hover-effect">
+			Counter 2
+		</div>
 	</div>
 </div>
 
 <style>
 	.table-header {
-		background: #0000000d;
+		background-color: #0000000d;
+	}
+	.hover-effect {
+		overflow: hidden;
+	}
+	.hover-effect:hover,
+	.hover-effect:active {
+		background-color: #0000000d;
 	}
 </style>
