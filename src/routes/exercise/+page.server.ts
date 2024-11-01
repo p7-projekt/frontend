04849515f31f5a4deabeef09e043e6c254346df2
Andreas Würtz <urintrolden@gmail.com>
2024-11-01 07:33:27 +0100

    get exercise from api

--- src/routes/exercise/+page.server.ts
+++ src/routes/exercise/+page.server.ts
@@ -3,10 +3,31 @@ import { setError, superValidate } from 'sveltekit-superforms';
 import { fail, redirect } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
 import { formSchema } from './schema';
+import { fetchExerciseData } from '$lib/requestHandler';
 
-export const load: PageServerLoad = async () => {
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const api_version = import.meta.env.VITE_V1;
+
+export const load: PageServerLoad = async ({ cookies }) => {
+	const access_token: string = cookies.get('access_token') || '';
+	const refresh_token: string = cookies.get('refresh_token') || '';
+	const exerciseId = new URLSearchParams(window.location.search).get('id') || 'XXX';
+
+
+	const response = await fetch(`${backendUrl}${api_version}/exercises/${exerciseId}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${access_token}`	
+	}});
+
+	const responseBody = await response.text();
+	const jsonResponse = JSON.parse(responseBody)
+	console.log(jsonResponse)
+	
 	return {
-		form: await superValidate(zod(formSchema))
+		form: await superValidate(zod(formSchema)), jsonResponse
 	};
 };
 
