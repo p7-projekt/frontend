470c4495d651943e29b932087ebfff3b135eca5f
EMojoKrogh <ekrogh18@student.aau.dk>
2024-11-06 14:05:14 +0100

    changed cookie deletion policy

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -3,8 +3,8 @@ import type { RequestHandler } from './$types';
 
 // Internal API endpoint to log the user out
 export const POST: RequestHandler = ({ cookies }) => {
-	cookies.delete('access_token', { path: '/' });
-	cookies.delete('refresh_token', { path: '/' });
+	cookies.delete('access_token', { path: '/', secure: false });
+	cookies.delete('refresh_token', { path: '/', secure: false });
 
 	return json({ message: 'User successfully logged out' }, { status: 200 });
 };
