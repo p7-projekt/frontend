ebca9706d4d4f5ae96d41ef2b1587a46bf8db53b
EMojoKrogh <ekrogh18@student.aau.dk>
2024-12-01 13:18:05 +0100

    join works for version 2 now

--- src/routes/join/+page.server.ts
+++ src/routes/join/+page.server.ts
@@ -15,7 +15,6 @@ export async function load({ parent }) {
 
 export const actions = {
 	joinAnon: async ({ request, cookies }) => {
-		console.log('one-off session');
 		const form = await request.formData();
 
 		const nickname = form.get('nickname');
@@ -29,7 +28,6 @@ export const actions = {
 		const validation = joinAnonSchema.safeParse(joinCode);
 
 		if (!validation.success) {
-			console.log(validation.error);
 			// Return the first validation error message if validation fails
 			return fail(400, { error: validation.error.errors[0].message });
 		}
@@ -79,7 +77,6 @@ export const actions = {
 		const validation = joinStudentSchema.safeParse(joinCode);
 
 		if (!validation.success) {
-			console.log(validation.error);
 			// Return the first validation error message if validation fails
 			return fail(400, { error: validation.error.errors[0].message });
 		}
@@ -92,56 +89,14 @@ export const actions = {
 			refresh_token,
 			cookies
 		);
-		console.log(response);
 		const responseData = await response.json();
-		console.log(responseData);
 
 		if (response.ok) {
 			if (responseData.joinedType === 2) {
-				throw redirect(303, `/classroom/${responseData.classroomId}`);
+				throw redirect(303, `/classroom/${responseData.joinedId}`);
 			} else throw redirect(303, '/session');
-		} else if (
-			responseData.errors.Errors[0] === 'Already joined' ||
-			responseData.errors.Errors[0] === 'Student already joined classroom'
-		) {
-			throw redirect(303, '/');
+		} else {
+			return fail(500, { message: 'Server error. Please try again later' });
 		}
-
-		// const response = await fetch(`${backendUrl}/join`, {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 		Authorization: `Bearer ${access_token}`
-		// 	},
-		// 	body: JSON.stringify(joinCode)
-		// });
-
-		// 	const responseData = await response.json();
-		// 	console.log(response);
-		// 	if (!response.ok) {
-		// 		if (responseData.errors?.Errors[0] === 'Already joined') {
-		// 			throw redirect(303, '/session/');
-		// 		}
-		// 		return fail(400, { error: responseData.errors?.SessionCode?.[0] || 'Invalid code' });
-		// 	}
-
-		// 	if (access_token == null) {
-		// 		const token = responseData.token;
-		// 		if (!token) {
-		// 			return fail(500, { message: 'Server error. Please try again later.' });
-		// 		}
-		// 		const token_expiration = responseData.expiresAt;
-
-		// 		const expires_at: Date = new Date(token_expiration);
-		// 		cookies.set('anon_token', token, {
-		// 			path: '/',
-		// 			httpOnly: true,
-		// 			secure: false,
-		// 			sameSite: 'strict',
-		// 			expires: expires_at
-		// 		});
-		// 	}
-
-		// 	throw redirect(303, '/session');
 	}
 };
