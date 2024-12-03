8f5547285a618d8c1522a1c9d06a1c572cbda8f5
Andreas Würtz <urintrolden@gmail.com>
2024-12-03 08:50:35 +0100

    fix test result display to comply with backend format

--- src/components/Tests/TestResultsStudent.svelte
+++ src/components/Tests/TestResultsStudent.svelte
@@ -5,7 +5,7 @@
 		id: number;
 		testResult: string;
 		cause?: string;
-		details?: { inputParameters: any[]; actual: any; expected: any };
+		details?: any;
 	}[];
 </script>
 
@@ -19,6 +19,10 @@
 				<p>Test ID: {test.id}</p>
 				<p>Result: {test.testResult}</p>
 				{#if test.testResult === 'failure'}
+					{#if typeof test.details === "string"}
+						<p>details: {test?.details} </p>
+					{/if}
+					{#if test?.details?.inputParameters !== undefined}
 					<p>Cause: {test.cause}</p>
 					<ul>
 						<li>
@@ -30,6 +34,7 @@
 						<li>Actual Output: {test?.details?.actual}</li>
 						<li>Expected Output: {test?.details?.expected}</li>
 					</ul>
+					{/if}
 				{/if}
 			</Tooltip.Content>
 		</Tooltip.Root>
